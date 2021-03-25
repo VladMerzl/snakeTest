44b15962aca3b46a701c32289761f7a44d9acfb6
Vlad <vlad-merzl@mail.ru>
2021-03-26 00:09:23 +0400

    lesson4 - constructors, this-word

--- snakeTest/snakeTest/Point.cs
+++ snakeTest/snakeTest/Point.cs
@@ -11,6 +11,18 @@ namespace snakeTest
         public int y;
         public char sym;
 
+        public Point()
+        {
+            //constructor
+        }
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
